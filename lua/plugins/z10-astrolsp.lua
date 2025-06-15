061d88b4db89399d93ee3b87e6273ba2d9fda0da
Seongjun Kim <bus710@gmail.com>
2025-06-15 15:32:44 -0700

    progress

--- lua/plugins/z10-astrolsp.lua
+++ lua/plugins/z10-astrolsp.lua
@@ -25,6 +25,7 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
+          "zig",
           "go",
           "rs",
           "ex",
