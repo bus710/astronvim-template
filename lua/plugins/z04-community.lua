68448a39e6af23b9bd2aec7dc9f92256de3f9909
Seongjun Kim <bus710@gmail.com>
2025-09-02 18:09:46 -0700

    enable elixir

--- lua/plugins/z04-community.lua
+++ lua/plugins/z04-community.lua
@@ -18,7 +18,7 @@ return {
   { import = "astrocommunity.pack.gleam" },
   { import = "astrocommunity.pack.bash" },
   --
-  -- { import = "astrocommunity.pack.elixir-phoenix" },
+  { import = "astrocommunity.pack.elixir-phoenix" },
 
   { import = "astrocommunity.pack.elm" },
 
