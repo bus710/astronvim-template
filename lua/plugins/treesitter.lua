5606909b24953add6b10cb515e98acc0e08fd3d9
bus710 <bus710@gmail.com>
2026-01-08 13:25:41 -0800

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,10 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "main", -- Specify the main branch
-  commit = "2ba5ec184609a96b513bf4c53a20512d64e27f39", -- https://github.com/nvim-treesitter/nvim-treesitter/commit/2ba5ec184609a96b513bf4c53a20512d64e27f39
-  build = ":TSUpdate",
   opts = {
     ensure_installed = {
       "lua",
