cd9f5e4c86914717e3535d378e52b85406029497
bus710 <bus710@gmail.com>
2026-01-08 13:17:04 -0800

    update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main", -- Specify the main branch
+  commit = "2ba5ec184609a96b513bf4c53a20512d64e27f39", -- https://github.com/nvim-treesitter/nvim-treesitter/commit/2ba5ec184609a96b513bf4c53a20512d64e27f39
   build = ":TSUpdate",
   opts = {
     ensure_installed = {
